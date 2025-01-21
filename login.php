6c41500d95e8a822cb984d71323a0827e7c8cf55
ezch74 <charlesyagazie95@gmail.com>
2025-01-21 17:14:56 +0100

    login kr8

--- login.php
+++ login.php
@@ -10,18 +10,10 @@ include("includes/head.php");
             <div class="card">
                 <div class="card-body">
                     <div class="card-header bg-white text-center border-bottom-0 d-flex">
-                        <h2 class="justify-content-center text-center border-bottom-0">Create Your Account</h2>
+                        <h2 class="justify-content-center text-center border-bottom-0">Welcome back</h2>
                     </div>
                     <form action="" method="post" class="p-5">
                         <div class="row">
-                            <div class="col-md-6 mb-2">
-                                <label class="ms-3 pb-1" for="fName"><strong>First Name</strong></label>
-                                <input type="text" name="fName" placeholder="First Name" class="form-control snup-in">
-                            </div>
-                            <div class="col-md-6 mb-2">
-                                <label class="ms-3 pb-1" for="lName"><strong>Last Name</strong></label>
-                                <input type="text" name="lName" placeholder="Last Name" class="form-control snup-in">
-                            </div>
                             <div class="col-md-12 mb-2">
                                 <label class="ms-3 pb-1" for="lName"><strong>Email</strong></label>
                                 <input type="email" name="email" placeholder="Email" class="form-control snup-in">
@@ -31,17 +23,11 @@ include("includes/head.php");
                                 <input type="password" name="pass" placeholder="Password" id="password" class="form-control snup-in">
                                 <i class="bx bx-show position-absolute" id="togglePass" style="top:54%; right:45px; cursor:pointer; tranform:translateY(-50%);"></i>    
                             </div>
-                            <div class="form-check justify-content-center my-2">
-                                <div class="ps-5">
-                                    <input class="form-check-input me-1" type="checkbox" id="check1" name="agree" value="">
-                                    <label for="check1" class="form-check-label"><strong>I Agree To All Terms, Privacy Policy and Fee</strong></label>
-                                </div>
-                            </div>
-                            <div class="col-md-12 mt-1">
-                                <button type="submit">Sign Up</button>
+                            <div class="col-md-12 mt-5">
+                                <button type="submit">Log In</button>
                             </div>
                             <div class="col-md-12 mt-1 ms-2">
-                                <p>Already have an account? <a href="login.php" class="agree-link">log in</a></p>
+                                <p>Don't have an account? <a href="signup.php" class="agree-link">create an account</a></p>
                             </div>
                         </div>
                     </form>
